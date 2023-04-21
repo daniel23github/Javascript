9f8e54c3d2d4f12ebc8ac6dae9aeeab58ce76610
daniel23github <daniel2005fevereiro@gmail.com>
2023-04-21 18:29:22 -0300

    analisador de números

---
+++ Aula16ex/modelo/script.js
@@ -0,0 +1,57 @@
+let numeros = []
+
+function adicionar() {
+    let res = document.getElementById('res')
+    res.innerHTML = ''
+    let n = Number(document.getElementById('inum').value)
+    let sel = document.getElementById('isel')
+    if (numeros.indexOf(n) != -1 || n.length == 0 || n < 1 || n > 100) {
+        alert('Valor invalido!')
+    } 
+
+    else {
+        numeros.push(n)
+        let opt = document.createElement('option')
+        opt.text = `Valor ${n} adicionado.`
+        sel.appendChild(opt)
+
+    }
+}
+
+function finalizar() {
+    if (numeros.length == 0) {
+        alert('Adicione Valores')
+    }
+
+    else {
+        let maior = 0
+        let menor = 0
+        let s = 0
+        for (let n in numeros) {
+            if (n == 0) {
+                maior = numeros[n]
+                menor = numeros[n]
+            }
+
+            else {
+                if (numeros[n] > maior) {
+                    maior = numeros[n]
+                }
+
+                if (numeros[n] < menor) {
+                    menor = numeros[n]
+                }
+            }
+            s += numeros[n]
+        }
+
+        let res = document.getElementById('res')
+        res.innerHTML = `<p>Ao todo, temos ${numeros.length} números cadastrados.</p>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}</p>`
+        res.innerHTML += `<p>Somando todos os valores, temos ${s}`
+        res.innerHTML += `<p>A média dos valores digitados é ${s / numeros.length}`
+    
+    }
+
+}
