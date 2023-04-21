4b0d291e75d1c50635317ca8b0b9a8775b6fc246
daniel23github <daniel2005fevereiro@gmail.com>
2023-04-21 13:51:33 -0300

    Update script.js

--- Aula12ex/modelo - Copia/script.js
+++ Aula12ex/modelo - Copia/script.js
@@ -15,39 +15,49 @@ function verificar() {
         img.setAttribute('id', 'foto')
         if (sexo[0].checked) {
             genero = 'homem'
+
             if (idade >= 0 && idade < 11) {
                 img.setAttribute('src', 'imagens/crianca-homem.png')
+
             }
 
             else if (idade < 21) {
                 img.setAttribute('src', 'imagens/jovem-homem.png')
+
             }
 
             else if (idade < 50) {
                 img.setAttribute('src', 'imagens/adulto-homem.png')
+
             }
 
             else {
                 img.setAttribute('src', 'imagens/idoso-homem.png')
+
             }
         }
 
         else {
+            
             genero = 'mulher'
             if (idade >= 0 && idade < 11) {
                 img.setAttribute('src', 'imagens/crianca-mulher.png')
+
             }
 
             else if (idade < 21) {
                 img.setAttribute('src', 'imagens/jovem-mulher.png')
+
             }
 
             else if (idade < 50) {
                 img.setAttribute('src', 'imagens/adulto-mulher.png')
+
             }
 
             else {
                 img.setAttribute('src', 'imagens/idoso-mulher.png')
+
             }
         }
         res.innerHTML = `Detectamos ${genero} com ${idade} anos`
